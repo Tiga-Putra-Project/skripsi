739a135918cc7a2fda0443c551e911b99d049454
Fayakun30 <namanah01@gmail.com>
2024-06-21 03:56:05 +0800

    Membuat Form Untuk menampilkan pencarian tiket

--- resources/views/homepage/pages/show_tiket.blade.php
+++ resources/views/homepage/pages/show_tiket.blade.php
@@ -1,17 +1,11 @@
 @extends('layouts.main')
 @section('main')
 
-    {{-- Pesan Tiket Kapal --}}
+    {{-- tampilkan tiket kapal --}}
     <div class="container-fluid booking py-5">
         <div class="container py-5">
             <div class="row g-5 align-items-center">
-                <div class="col-lg-6">
-                    <h5 class="section-booking-title pe-3">Booking</h5>
-                    <h1 class="text-white mb-4">Booking Tiket Kapal</h1>
-                    <p class="text-white mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur maxime ullam esse fuga blanditiis accusantium pariatur quis sapiente, veniam doloribus praesentium? Repudiandae iste voluptatem fugiat doloribus quasi quo iure officia.
-                    </p>
-                </div>
-                <div class="col-lg-6">
+                <div class="col-lg-12">
                     <h1 class="text-white mb-3">Cari Tiket yang Anda Inginkan</h1>
                     <p class="text-white mb-4">Get On Your First Adventure Trip With Travela. Get More Deal Offers Here.</p>
                     <form>
@@ -36,17 +30,6 @@
                             </div>
                             <div class="col-md-6">
                                 <div class="form-floating">
-                                    <select class="form-select bg-white border-0" id="CategoriesSelect">
-                                        <option value="1">Pejalan Kaki</option>
-                                        <option value="2">Sepeda</option>
-                                        <option value="3">Sepeda Motor</option>
-                                        <option value="3">Mobil</option>
-                                    </select>
-                                    <label for="CategoriesSelect">Tipe Tiket</label>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-floating">
                                     <input type="email" class="form-control bg-white border-0" id="email" placeholder="Your Email">
                                     <label for="email">Penumpang</label>
                                 </div>
@@ -58,8 +41,7 @@
                                 </div>
                             </div>
                             <div class="col-12">
-                                <button class="btn btn-primary text-white w-100 py-3" type="submit">Cari Sekarang
-                                </button>
+                                <button class="btn btn-primary text-white w-100 py-3" type="submit">Cari Sekarang!</button>
                             </div>
                         </div>
                     </form>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -130,9 +130,9 @@
         <!-- Copyright Start -->
         <div class="container-fluid copyright text-body py-4">
             <div class="container">
-                <div class="row g-4 align-items-center">
-                    <div class="col-md-6 text-center text-md-end mb-md-0">
-                        <i class="fas fa-copyright me-2"></i><a class="text-white" href="#">Tiga Putra Travel</a>, All right reserved.
+                <div class="row g-4 align-items-left">
+                    <div class="col-md-6 text-center text-md-start mb-md-0">
+                        <i class="fas fa-copyright me-2"></i><a class="text-white" href="{{ route('homepage.landing') }}">2024 Tiga Putra Travel</a>, All right reserved.
                     </div>
                     <div class="col-md-6 text-center text-md-start">
                         <!--/*** This template is free as long as you keep the below author’s credit link/attribution link/backlink. ***/-->
@@ -141,7 +141,6 @@
                     </div>
                     </div>
                 </div>
-
         </div>
         <!-- Copyright End -->
 
